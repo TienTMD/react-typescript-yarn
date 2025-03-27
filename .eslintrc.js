cb363819c2c9a0f9622f0a142128e9580cbe2f5b
Tiến <77319510+TienTMD@users.noreply.github.com>
2025-03-27 13:08:23 +0700

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,35 +16,40 @@ module.exports = {
   },
   ignorePatterns: ['vendor/', 'build/', 'public/build', 'coverage/'],
   root: true,
-  "rules": {
-    "linebreak-style": 0,
-    "function-paren-newline": "off",
-    "implicit-arrow-linebreak": "off",
-    "comma-dangle": ["error", "only-multiline"],
-    "import/prefer-default-export": "off",
-    "import/no-extraneous-dependencies": "off",
-    "react/require-default-props": "off",
-    "react/function-component-definition": [
-      "warn",
+  settings: {
+    react: {
+      version: 'detect', // change this to a specific version if it cannot be detected
+    },
+  },
+  rules: {
+    'linebreak-style': 0,
+    'function-paren-newline': 'off',
+    'implicit-arrow-linebreak': 'off',
+    'comma-dangle': ['error', 'only-multiline'],
+    'import/prefer-default-export': 'off',
+    'import/no-extraneous-dependencies': 'off',
+    'react/require-default-props': 'off',
+    'react/function-component-definition': [
+      'warn',
       {
-        "namedComponents": [
-          "function-declaration",
-          "function-expression",
-          "arrow-function"
-        ],
-        "unnamedComponents": ["function-expression", "arrow-function"]
-      }
+        namedComponents: ['function-declaration', 'function-expression', 'arrow-function'],
+        unnamedComponents: ['function-expression', 'arrow-function'],
+      },
     ],
-    "object-curly-newline": "off",
-    "jsx-a11y/label-has-associated-control": [
-      "error",
+    'object-curly-newline': 'off',
+    'jsx-a11y/label-has-associated-control': [
+      'error',
       {
-        "required": {
-          "some": ["nesting", "id"]
-        }
-      }
+        required: {
+          some: ['nesting', 'id'],
+        },
+      },
     ],
-    "func-names": ["error", "as-needed"],
-    "arrow-body-style": "off"
-  }
+    'func-names': ['error', 'as-needed'],
+    'arrow-body-style': 'off',
+    'prefer-destructuring': 'off',
+    'no-use-before-define': 'off',
+    '@typescript-eslint/no-use-before-define': 'off',
+    'import/order': 'off',
+  },
 };
